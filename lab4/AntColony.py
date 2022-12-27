a7f4e9045d0fcf36596fd6fd33ff747c94c8546b
Sonya Kondratskaya <sonyakondratskaya@gmail.com>
2022-12-27 19:49:40 +0100

    add lab4 files

---
+++ lab4/AntColony.py
@@ -0,0 +1,73 @@
+import random as rp
+import numpy as np
+from numpy.random import choice as np_choice
+
+class AntColony(object):
+
+    def __init__(self, distances, n_ants, n_iterations, decay, alpha, beta):
+
+        self.distances = distances
+        self.pheromone = np.ones(self.distances.shape) / len(distances)
+        self.all_inds = range(len(distances))
+        self.n_ants = n_ants
+        self.n_iterations = n_iterations
+        self.decay = decay
+        self.alpha = alpha
+        self.beta = beta
+        self.Lmin = self.gen_path_distance(self.gen_path(0)) #
+
+    def run(self):
+        shortest_path = None
+        all_time_shortest_path = ("placeholder", np.inf)
+        for i in range(self.n_iterations):
+            print('\rCompleted: {}%'.format((i+1)*100/self.n_iterations), end='')
+            all_paths = self.gen_all_paths()
+            self.spread_pheronome(all_paths)
+            shortest_path = min(all_paths, key=lambda x: x[1])
+
+            if shortest_path[1] < all_time_shortest_path[1]:
+                all_time_shortest_path = shortest_path
+            self.pheromone = self.pheromone * self.decay
+
+        return all_time_shortest_path
+
+    def gen_all_paths(self):
+        all_paths = []
+        for i in range(self.n_ants):
+            path = self.gen_path(0)
+            all_paths.append((path, self.gen_path_distance(path)))
+        return all_paths
+
+    def gen_path_distance(self, path):
+        total_dist = 0
+        for ele in path:
+            total_dist += self.distances[ele]
+        return total_dist
+
+    def gen_path(self, start):
+        path = []
+        visited = set()
+        visited.add(start)
+        prev = start
+        for i in range(len(self.distances) - 1):
+            move = self.pick_move(self.pheromone[prev], self.distances[prev], visited)
+            path.append((prev, move))
+            prev = move
+            visited.add(move)
+        path.append((prev, start)) # going back to home
+        return path
+
+    def spread_pheronome(self, all_paths):
+        sorted_paths = sorted(all_paths, key=lambda x: x[1])
+        for path, dist in sorted_paths:
+            for move in path:
+                self.pheromone[move] += self.Lmin / self.distances[move]
+
+
+    def pick_move(self, pheromone, dist, visited):
+        pheromone = np.copy(pheromone)
+        pheromone[list(visited)] = 0
+        row = pheromone ** self.alpha * (( 1.0 / dist) ** self.beta)
+        norm_row = row / row.sum()
+        move = np_choice(self.all_inds, 1, p=norm_row)[0]
+        return move
